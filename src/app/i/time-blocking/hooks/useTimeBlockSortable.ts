08a07ef1e2e98c9dd4867af541b249b7bb9f1837
Данис Иртуганов <danisirtuganov@gmail.com>
2024-04-12 16:17:01 +0300

    Init project

---
+++ src/app/i/time-blocking/hooks/useTimeBlockSortable.ts
@@ -0,0 +1,19 @@
+import { UniqueIdentifier } from '@dnd-kit/core'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import type { CSSProperties } from 'react'
+
+export function useTimeBlockSortable(id: UniqueIdentifier) {
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable({ id })
+
+	const style: CSSProperties = {
+		transform: CSS.Transform.toString(transform)?.replace(
+			/scaleY\(\d+\.?\d*\)/,
+			'scaleY(1)'
+		),
+		transition
+	}
+
+	return { attributes, listeners, setNodeRef, style }
+}
